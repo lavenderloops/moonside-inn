41383893429a886575ef00c347e5babd5e941930
lavenderloops <khatiti.harper@gmail.com>
2025-03-07 17:19:44 -0800

    add p5r page

--- scripts/script.js
+++ scripts/script.js
@@ -12,3 +12,26 @@ for (i = 0; i < coll.length; i++) {
     }
   });
 } 
+
+
+
+
+var spoil = document.getElementsByClassName("spoiler");
+var i;
+
+for (i = 0; i < spoil.length; i++) {
+  spoil[i].addEventListener("click", function () {
+    // Toggle the 'active' class
+    this.classList.toggle("active");
+
+    var content = this
+    // Check if the 'active' class is present and change the background color accordingly
+    if (this.classList.contains("active")) {
+      this.style.backgroundColor = "yellow"; // Reveals the text with a background color change (you can choose the color)
+      this.style.color = "black"; // Optionally, change the text color to make it more visible
+    } else {
+      this.style.backgroundColor = "black"; // Original background color (or color you want when the text is hidden)
+      this.style.color = "black"; // Keep text color black when it's hidden
+    }
+  });
+}
