acc1fbd32d2fdc9b301e8ab180b15b45eb8d361f
lavenderloops <khatiti.harper@gmail.com>
2025-03-14 17:20:54 -0700

    add image modal to bg3 pg

--- scripts/script.js
+++ scripts/script.js
@@ -11,4 +11,79 @@ for (i = 0; i < coll.length; i++) {
       content.style.display = "block";
     }
   });
+<<<<<<< Updated upstream
 } 
+=======
+}
+
+// load logbook
+
+function loadLogbook() {
+  const xhttp = new XMLHttpRequest();
+  xhttp.onload = function () {
+    document.getElementById("logbook").innerHTML =
+      this.responseText;
+  }
+  xhttp.open("GET", "text/logbook.xml");
+  xhttp.send();
+}
+
+loadLogbook()
+
+// lastfm loader
+
+{
+  let user = 'harperka';
+  let url = 'https://crossorigin.me/https://lastfm-last-played.biancarosa.com.br/' + user + '/latest-song';
+  let song = document.querySelector('#song');
+  fetch(url)
+    .then(function (response) {
+      return response.json()
+    }).then(function (json) {
+      song.innerHTML = json['track']['name'] + ' - ' + json['track']['artist']['#text'];
+    });
+}
+
+// modal
+
+// Open the Modal
+function openModal() {
+  document.getElementById("galleryModal").style.display = "block";
+}
+
+// Close the Modal
+function closeModal() {
+  document.getElementById("galleryModal").style.display = "none";
+}
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("demo");
+  var captionText = document.getElementById("caption");
+  if (n > slides.length) { slideIndex = 1 }
+  if (n < 1) { slideIndex = slides.length }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+  captionText.innerHTML = dots[slideIndex - 1].alt;
+}
+>>>>>>> Stashed changes
