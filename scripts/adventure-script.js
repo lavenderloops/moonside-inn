e1683728b2217f8ff7c9862013990fa3c7765d7c
lavenderloops <khatiti.harper@gmail.com>
2025-05-20 15:34:45 -0700

    add button functionality

--- scripts/adventure-script.js
+++ scripts/adventure-script.js
@@ -4,12 +4,12 @@ var milkbun = document.getElementById("milkbun")
 milkbun.addEventListener("mouseover", function () {
     milkbun.style.transition = ("transform 0.2s ease")
     milkbun.style.transform = "rotate(6deg) scale(.9)";
-    milkbun.src = "images/milkbun-1.png";
+    milkbun.src = "/adventure-log/images/milkbun-1.png";
 })
 
 milkbun.addEventListener("mouseout", function () {
     milkbun.style.transform = "rotate(0deg)";
-    milkbun.src = "images/milkbun.png";
+    milkbun.src = "/adventure-log/images/milkbun.png";
 })
 
 
@@ -18,13 +18,34 @@ var dino = document.getElementById("dino");
 dino.addEventListener("mouseover", function () {
     dino.style.transition = "transform 0.3s ease"; // Add transition
     dino.style.transform = "rotate(6deg) scale(1.2)"
-    dino.src = "images/dino-1.png";
+    dino.src = "/adventure-log/images/dino-1.png";
 });
 
 dino.addEventListener("mouseout", function () {
-    dino.src = "images/dino.png";
+    dino.src = "/adventure-log/images/dino.png";
     dino.style.transform = "rotate(0deg)"
 });
 
 // planner buttons
+// var planner = document.getElementsByClassName("planner");
+var i;
+const buttons = document.querySelectorAll('button');
 
+
+for (i = 0; i < buttons.length; i++){
+    buttons[i].addEventListener('click', function(onButtonClick) {
+        const clickedButtonId = event.target.id;
+        if (clickedButtonId === '0417') {
+            this.style.backgroundColor = "red"
+        }
+    });
+}
+
+
+// for (i = 0; i < buttons.length; i++) {
+//     // Correctly pass the function reference
+//     buttons[i].addEventListener('click', function(event) {
+//         // 'this' inside a regular function refers to the element that triggered the event
+//         this.style.transform = "rotate(2deg)"; 
+//     });
+// }
