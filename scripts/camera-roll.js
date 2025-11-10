2b00588136700829562259f1038e441397af6072
lavenderloops <khatiti.harper@gmail.com>
2025-11-09 22:53:06 -0800

    add weekly log

--- scripts/camera-roll.js
+++ scripts/camera-roll.js
@@ -1,11 +1,12 @@
 document.addEventListener("DOMContentLoaded", function () {
     const cameraRoll = document.querySelector(".camera-roll");
     const images = [
+        "/images/camera-roll/reserve.png",
         "/images/camera-roll/clouds.png",
         "/images/camera-roll/sunset.png",
         "/images/camera-roll/hilltop.png",
         "/images/camera-roll/lumitube.png",
-        "/images/camera-roll/tater.png"
+        "/images/camera-roll/bunbuns.png",
     ];
 
     let currentIndex = 0;
